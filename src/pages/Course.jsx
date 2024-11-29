bd90eadbc96eac1a9ef98de0be3b51c196356b4b
raghab007 <pokhrelraghab3@gmail.com>
2024-11-29 17:19:32 +0545

    improve

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import BasicCard from '../components/CourseCard';
-export default function Course(){
-    return (<div> 
-        
-        <div style={{display:'flex',justifyContent:'space-around',marginTop:'60px'}}>
-        {/* <VideoCard description={"Video"} title={"Video"} videoUrl={"/videos/video1.mp4"}></VideoCard>
-        <VideoCard description={"Video"} title={"Video"} videoUrl={"/videos/video1.mp4"}></VideoCard>
-        <VideoCard description={"Video"} title={"Video"} videoUrl={"/videos/video1.mp4"}></VideoCard> */}
+import springbootImg from '../assets/springboot.png';
+import mernImg from '../assets/mern.png';
+import uiuxImg from '../assets/ui_ux.png';
+import javaImg from '../assets/java.png';
+import jsImg from '../assets/js.jpg';
+import pythonImg from '../assets/python.jpg';
 
-        <BasicCard price={'2000'} description={'Html, CSS, Javascript, React'} title={'Full web development'} imageSrc={'public/springboot.png'}></BasicCard>
-        <BasicCard price={'3000'} description={'Java, Jsp, Servlet,Spring Boot'} title={'Java masterclass'}  imageSrc={'public/mern.png'}></BasicCard>
-        <BasicCard price={'1000'} description={'Front end design'} title={'UI/UX'}  imageSrc={'public/ui_ux.png'}></BasicCard>
-        </div>
-        <div style={{display:'flex',justifyContent:'space-around',marginTop:'40px'}}>
-          
-        <BasicCard price={'2000'} description={'Html, CSS, Javascript, React'} title={'Full web development'} imageSrc={'public/java.png'}></BasicCard>
-        <BasicCard price={'3000'} description={'Java, Jsp, Servlet,Spring Boot'} title={'Java masterclass'}  imageSrc={'public/js.jpg'}></BasicCard>
-        <BasicCard price={'1000'} description={'Front end design'} title={'UI/UX'}  imageSrc={'public/python.jpg'}></BasicCard>
-        </div>
-       </div>
-    )
+export default function Course() {
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '60px' }}>
+        <BasicCard price={'2000'} description={'Html, CSS, Javascript, React'} title={'Full web development'} imageSrc={springbootImg} />
+        <BasicCard price={'3000'} description={'Java, Jsp, Servlet, Spring Boot'} title={'Java masterclass'} imageSrc={mernImg} />
+        <BasicCard price={'1000'} description={'Front end design'} title={'UI/UX'} imageSrc={uiuxImg} />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '40px' }}>
+        <BasicCard price={'2000'} description={'Html, CSS, Javascript, React'} title={'Full web development'} imageSrc={javaImg} />
+        <BasicCard price={'3000'} description={'Java, Jsp, Servlet, Spring Boot'} title={'Java masterclass'} imageSrc={jsImg} />
+        <BasicCard price={'1000'} description={'Front end design'} title={'UI/UX'} imageSrc={pythonImg} />
+      </div>
+    </div>
+  );
 }
